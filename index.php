d7751c576e87031493c4267025d93080e9b6d4ee
Eric Corriel <ecorriel@sva.edu>
2018-10-15 01:32:29 -0400

    week 1 start

--- index.php
+++ index.php
@@ -5,60 +5,21 @@ include($_SERVER['DOCUMENT_ROOT'] . "/includes/formattop.php"); ?>
 
 			
 	<div class="page">
-		<div class="pagename"><a href='/'>DSD-4708-A</a></div>
+		<div class="pagename"><a href='/'>DSG-6051</a></div>
 		<div class="pagecontent">
-			<h3 class="unstack">Interaction Design, Coding and Content Strategy</h3>
-			<p>This course has three objectives: 1) Teach the technical coding skills (HTML, CSS, and Javascript) required to create contemporary websites for desktop, tablet, and mobile devices. 2) Make students better design thinkers by challenging them to articulate strategic thinking in the form of wireframes, sitemaps, content and social media strategies, client presentations, and writing assignments. 3) Convey best design practices for the web and knowledge about how the Internet works in general.  Underlying this course is the belief that designers who are able to think clearly and strategically about design as well as execute complex technical coding projects will be better equipped to succeed in our evolving digital landscape.</p>
+			<h3 class="unstack">UI/UX Workshop</h3>
+			<p>This workshop has three objectives: 1) introduce students to contemporary UX/UI prototyping tools such as <a href='http://sketchapp.com' target='_blank' >Sketch</a> and <a href='http://principleformac.com/' target='_blank'>Principle</a>, 2) integrate those tools into their personal projects, and 3) prime students on how to prepare their projects for developers.</p>
 			<p><a href='http://www.ericcorriel.com' target='_blank'>Eric Corriel</a> | <a href='mailto:ecorriel@sva.edu'>ecorriel@sva.edu</a></p>
 			
 			
 			<h3 class="unstack">Fall 2018</h3>
 			<ul>
-				<li><a href='/2018-2019/week-1'>Week 1 : Class introduction + Intro to HTML and CSS</a></li>
-				<li><a href='/2018-2019/week-2'>Week 2 : E-blast design and creation with Photoshop</a></li>
-				<li><a href='/2018-2019/week-3'>Week 3 : Client feedback and annotated mockups</a> + Genspace client brief</a> (<em>do not miss!</em>)</li>
-				<li><a href='/2018-2019/week-4'>Week 4 : E-blast design and creation with HTML</a></li>
-				<li><a href='/2018-2019/week-5'>Week 5 : The HTML/CSS box model and CSS Grid</a></li>
-				<li>Week 6 : The HTML/CSS box model review</a></li>
-				<li>Week 7 : Content strategy</a></li>
-				<li>Week 8 : Coding an HTML page from scratch</a></li>
-				<li>Week 9 : Web typography</a></li>
-				<li>Week 10 : Sildeshows </a></li>
-				<li>Week 11 : Wireframing and navigation with jQuery and JSON</a></li>
-				<li>Week 12 : Genspace homepage redesign</a></li>
-				<li>Week 13 : Interactive Prototypes, Secret Sauce &amp; Google Maps</a></li>
-				<li>Week 14 : Let's learn about the Internet!</a></li>
-				<li>Week 15 : Genspace client presentations</a> (<em>do not miss!</em>)</li>
+				<li><a href='/2018-2019/week-1'>Week 1 : Workshop Introduction, Internet Pop Quiz I, Sketch I</a></li>
+				<li>Week 2 : Internet Pop Quiz II, Sketch II</a></li>
+				<li>Week 3 : How to Use a Computer, Prototyping for Developers I</li>
+				<li>Week 4 : Prototyping for Developers II</a></li>
 			</ul>
 
-			
-			<h3 class="unstack">Spring 2019</h3>
-			<ul>
-				<li>Week 1 : Class introduction, HTML/CSS review + Your resume I</a></li>
-				<li>Week 2 : Your resume II + Intro to Responsive Design</a></li>
-				<li>Week 3 : Terminal, Web servers, FTP, code editors</a></li>
-				<li>Week 4 : Responsive Design with Bootstrap I</a></li>
-				<li>Week 5 : Responsive Design with Bootstrap II, HTML forms</a></li>
-				<li>Week 6 : Your first freelance gig!</a></li>
-				<li>Week 7 : The MAMP stack</a></li>
-				<li>Week 8 : Freelancing + Interactive Prototyping with Sketch</a></li>
-				<li>Week 9 : Your website: wireframes + How to use a computer</a></li>
-				<li>Week 10 : Your website: design + PHP I</a></li>
-				<li>Week 11 : Your website: interactive prototypes + PHP II</a></li>
-				<li>Week 12 : Your website: coding + Domain names/hosting</a></li>
-				<li>Week 13 : Your website: coding + Javascript I</a></li>
-				<li>Week 14 : Your website: coding + CSS Animation</a></li>
-				<li>Week 15 : Final presentations + where to go from here...</a></li>
-			</ul>	
-
-			<h3 class="unstack">Resources</h3>
-			<ul>
-				<li><a href='/2018-2019/assets/00.presentations/genspace-presentation.pdf'>Genspace Presentation</a></li>
-				<li><a href='/resources/working-with-imgur.php'>How to work with Imgur</a></li>
-				<li><a href='/resources/how-to-code-css.php'>How to code CSS</a></li>
-				<li><a href='/resources/responsive-design.php'>Responsive design tips</a></li>
-				<li><a href='http://escapecodes.info/' target='_blank' >HTML Escape Codes</a></li>
-			</ul>
 		</div>
 
 	</div>
